ca4b4cc89fb3a3a713a625302a23990305bc349d
Kuszki <l.drozdz@openmailbox.org>
2017-02-22 19:08:49 +0100

    Create pl_PL translations

--- k-plots_pl.ts
+++ k-plots_pl.ts
@@ -6,142 +6,139 @@
     <message>
         <location filename="aboutdialog.ui" line="20"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O programie</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="30"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zamknij</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="71"/>
         <source>Program name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Program:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="88"/>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="105"/>
         <source>Mail:</source>
-        <translation type="unfinished"></translation>
+        <translation>E-mail:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="125"/>
         <source>Project slite:</source>
-        <translation type="unfinished"></translation>
+        <translation>Strona projektu:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="145"/>
         <source>License:</source>
-        <translation type="unfinished"></translation>
+        <translation>Licencja:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="165"/>
         <source>Powered by:</source>
-        <translation type="unfinished"></translation>
+        <translation>Wsparcie:</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="187"/>
         <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ten program jest wolnym oprogramowaniem: możesz go rozpowszechniać i/lub modyfikować na warunkach Powszechnej Publicznej Licencji GNU GPL opublikowanej przez Fundacje Wolnego Oprogramowania w wersji 3 lub (według uznania) dowolnej późniejszej.
+
+Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny – jednak BEZ ŻADNEJ GWARANCJI, nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. Bliższe informacje na ten temat można uzyskać z Powszechnej Licencji Publicznej GNU.</translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="202"/>
         <source>&lt;a href=&quot;http://www.fsf.org/&quot;&gt;See more&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=&quot;http://www.fsf.org/&quot;&gt;Dowiedz się więcej&lt;/a&gt;</translation>
     </message>
 </context>
 <context>
     <name>ChartWidget</name>
     <message>
-        <location filename="chartwidget.ui" line="20"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="chartwidget.ui" line="29"/>
+        <location filename="chartwidget.ui" line="26"/>
         <source>Plot function</source>
-        <translation type="unfinished"></translation>
+        <translation>Rysuj funkcję</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="34"/>
+        <location filename="chartwidget.ui" line="31"/>
         <source>Plot integral</source>
-        <translation type="unfinished"></translation>
+        <translation>Rysuj całkę</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="39"/>
+        <location filename="chartwidget.ui" line="36"/>
         <source>Plot derivative</source>
-        <translation type="unfinished"></translation>
+        <translation>Rysuj pochodną</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="44"/>
+        <location filename="chartwidget.ui" line="41"/>
         <source>Plot spectrum</source>
-        <translation type="unfinished"></translation>
+        <translation>Rysuj widmo</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="52"/>
+        <location filename="chartwidget.ui" line="49"/>
         <source>Show legend</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokaż legende</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="59"/>
+        <location filename="chartwidget.ui" line="56"/>
         <source>Zoom X</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaluj oś X</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="69"/>
+        <location filename="chartwidget.ui" line="66"/>
         <source>Zoom Y</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaluj oś Y</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="143"/>
+        <location filename="chartwidget.ui" line="140"/>
         <source>Abs</source>
-        <translation type="unfinished"></translation>
+        <translation>Moduł</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="151"/>
+        <location filename="chartwidget.ui" line="148"/>
         <source>Re</source>
-        <translation type="unfinished"></translation>
+        <translation>Część rzeczywista</translation>
     </message>
     <message>
-        <location filename="chartwidget.ui" line="159"/>
+        <location filename="chartwidget.ui" line="156"/>
         <source>Im</source>
-        <translation type="unfinished"></translation>
+        <translation>Część urojona</translation>
     </message>
     <message>
         <location filename="chartwidget.cpp" line="148"/>
         <source>Spectrum options</source>
-        <translation type="unfinished"></translation>
+        <translation>Elementy widma</translation>
     </message>
     <message>
-        <location filename="chartwidget.cpp" line="427"/>
+        <location filename="chartwidget.cpp" line="437"/>
         <source>Select file to save data</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz plik do zapisu danych</translation>
     </message>
     <message>
-        <location filename="chartwidget.cpp" line="427"/>
+        <location filename="chartwidget.cpp" line="437"/>
         <source>CSV files (*.csv)</source>
-        <translation type="unfinished"></translation>
+        <translation>Pliki CSV (*.csv)</translation>
     </message>
     <message>
-        <location filename="chartwidget.cpp" line="436"/>
+        <location filename="chartwidget.cpp" line="446"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Czas</translation>
     </message>
     <message>
-        <location filename="chartwidget.cpp" line="455"/>
+        <location filename="chartwidget.cpp" line="465"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
     <message>
-        <location filename="chartwidget.cpp" line="455"/>
+        <location filename="chartwidget.cpp" line="465"/>
         <source>Can&apos;t open selected file</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie można otworzyć wybranego pliku</translation>
     </message>
 </context>
 <context>
@@ -149,30 +146,25 @@ The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRAN
     <message>
         <location filename="functionsdialog.ui" line="14"/>
         <source>Function options</source>
-        <translation type="unfinished"></translation>
+        <translation>Właściwości funkcji</translation>
     </message>
     <message>
-        <location filename="functionsdialog.ui" line="20"/>
+        <location filename="functionsdialog.ui" line="23"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
     <message>
         <location filename="functionsdialog.cpp" line="86"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
 </context>
 <context>
     <name>FunctionsWidget</name>
     <message>
-        <location filename="functionswidget.ui" line="20"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="functionswidget.ui" line="38"/>
+        <location filename="functionswidget.ui" line="35"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj</translation>
     </message>
 </context>
 <context>
@@ -180,125 +172,136 @@ The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRAN
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>K-Plots</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="26"/>
         <source>Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Narzędzia</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="45"/>
         <source>Variables</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmienne</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="54"/>
         <source>Functions</source>
-        <translation type="unfinished"></translation>
+        <translation>Funkcje</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="63"/>
         <source>Plots</source>
-        <translation type="unfinished"></translation>
+        <translation>Wykresy</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="72"/>
         <source>Support</source>
-        <translation type="unfinished"></translation>
+        <translation>Wsparcie</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="97"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Otwórz</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="100"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="109"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="112"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="121"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O programie</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="124"/>
         <source>F1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="47"/>
+        <location filename="mainwindow.cpp" line="50"/>
         <source>From </source>
-        <translation type="unfinished"></translation>
+        <translation>Od </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="48"/>
+        <location filename="mainwindow.cpp" line="51"/>
         <source>to </source>
-        <translation type="unfinished"></translation>
+        <translation>do </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="49"/>
+        <location filename="mainwindow.cpp" line="52"/>
         <source>Plot </source>
-        <translation type="unfinished"></translation>
+        <translation>Rysuj </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="73"/>
         <source>Plot</source>
-        <translation type="unfinished"></translation>
+        <translation>Wykres</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="87"/>
+        <location filename="mainwindow.cpp" line="90"/>
         <source>This name is used by another function</source>
-        <translation type="unfinished"></translation>
+        <translation>Ta nazwa jest już w użyciu przez inną funkcję</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="99"/>
         <source>Error in line %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd w linii %1: %2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Save current environment into file</source>
+        <translation>Zapis środowiska do pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <location filename="mainwindow.cpp" line="180"/>
+        <source>XML Files (*.xml)</source>
+        <translation>Pliki XML (*.xml)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="180"/>
+        <source>Load environment from file</source>
+        <translation>Załaduj środowisko z pliku</translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindow.cpp" line="143"/>
+        <location filename="mainwindow.cpp" line="229"/>
         <source> sample(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform> próbkę</numerusform>
+            <numerusform> próbki</numerusform>
+            <numerusform> próbek</numerusform>
         </translation>
     </message>
 </context>
 <context>
     <name>PlotslistWidget</name>
     <message>
-        <location filename="plotslistwidget.ui" line="20"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="plotslistwidget.ui" line="36"/>
+        <location filename="plotslistwidget.ui" line="33"/>
         <source>Plot</source>
-        <translation type="unfinished"></translation>
+        <translation>Wykres</translation>
     </message>
     <message>
-        <location filename="plotslistwidget.ui" line="46"/>
+        <location filename="plotslistwidget.ui" line="43"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj</translation>
     </message>
     <message>
-        <location filename="plotslistwidget.cpp" line="140"/>
-        <location filename="plotslistwidget.cpp" line="142"/>
+        <location filename="plotslistwidget.cpp" line="150"/>
+        <location filename="plotslistwidget.cpp" line="152"/>
         <source>Plot %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Wykres %1</translation>
     </message>
 </context>
 <context>
@@ -306,12 +309,12 @@ The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRAN
     <message>
         <location filename="titlewidget.ui" line="84"/>
         <source>Dock</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokuj</translation>
     </message>
     <message>
         <location filename="titlewidget.ui" line="94"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zamknij</translation>
     </message>
 </context>
 <context>
@@ -319,40 +322,35 @@ The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRAN
     <message>
         <location filename="variablesdialog.ui" line="14"/>
         <source>Create variable</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj zmienną</translation>
     </message>
     <message>
         <location filename="variablesdialog.ui" line="24"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Wartość</translation>
     </message>
     <message>
         <location filename="variablesdialog.ui" line="47"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
 </context>
 <context>
     <name>VariablesWidget</name>
     <message>
-        <location filename="variableswidget.ui" line="20"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="variableswidget.ui" line="42"/>
+        <location filename="variableswidget.ui" line="39"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
     <message>
-        <location filename="variableswidget.ui" line="47"/>
+        <location filename="variableswidget.ui" line="44"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Wartość</translation>
     </message>
     <message>
-        <location filename="variableswidget.ui" line="57"/>
+        <location filename="variableswidget.ui" line="54"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj</translation>
     </message>
 </context>
 </TS>
